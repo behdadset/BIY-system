e5543a99f1bfd900eaf7f8173426d40334b43783
nicolezhangyt <nicolezhangyt91@gmail.com>
2019-05-18 22:38:42 +1000

    connect the product name to the product review

--- app/src/main/java/com/biy/reviewvia/MainActivity.java
+++ app/src/main/java/com/biy/reviewvia/MainActivity.java
@@ -1,19 +1,13 @@
 package com.biy.reviewvia;
 
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ListView;
-import android.widget.Toast;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.ArrayList;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -22,8 +16,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                         if(!mEnter.getText().toString().isEmpty()){
                             //st.makeText(MainActivity.this,"Enter successful",Toast.LENGTH_SHORT).show();
                             barcode = mEnter.getText().toString();
-                            new GetProduct().execute();
+                            new GetProduct().execute(barcode);
 
                         }else{
                             Toast.makeText(MainActivity.this,"Please enter the barcode ",Toast.LENGTH_SHORT).show();
@@ -82,17 +84,6 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
-
-
-        //for test only start
-        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ReviewsActivity.class));
-            }
-        });
-
-
     }
 
     @Override
@@ -138,35 +129,37 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MainActivity","scanned");
                 //Toast.makeText(this,"scanned:" + result.getContents(),Toast.LENGTH_LONG).show();
                 barcode = result.getContents();
-                new GetProduct().execute();
+                new GetProduct().execute(barcode);
                 ((TextView)findViewById(R.id.tv_prompt)).setText(result.getContents());
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    //Pasted
-    private String TAGapp = MainActivity.class.getSimpleName();
-    private ListView lv;
-
-    String productName="";
-
-
-    private class GetProduct extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
+    private void toReviewer(String productName) {
+        if (productName == null) {
+            Toast.makeText(this, "No matching data", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(MainActivity.this, ReviewsActivity.class);
+            intent.putExtra("product_name", productName);
+            startActivity(intent);
         }
 
+    }
+
+    private class GetProduct extends AsyncTask<String, Void, String> {
+
+        private static final String GET_PRODUCT_TAG = "tag_get_product";
+
         @Override
-        protected Void doInBackground(Void... arg0) {
+        protected String doInBackground(String... codes) {
             HttpHandler sh = new HttpHandler();
             // Making a request to url and getting response
-            String url = "https://api.upcitemdb.com/prod/trial/lookup?upc="+ barcode;
+            String url = "https://api.upcitemdb.com/prod/trial/lookup?upc="+ codes[0];
             String jsonStr = sh.makeServiceCall(url);
+            String result = null;
 
-            Log.e(TAGapp, "Response from url: " + jsonStr);
+            Log.i(GET_PRODUCT_TAG, "Response from url: " + jsonStr);
             if (jsonStr != null) {
                 try {
                     JSONObject jsonObj = new JSONObject(jsonStr);
@@ -176,50 +169,31 @@ public class MainActivity extends AppCompatActivity {
 
                     int i = 0;
                     JSONObject c = products.getJSONObject(i);
-                    String title = c.getString("title");
-                    productName = title;
-
+                    result = c.getString("title");
 
                 } catch (final JSONException e) {
-                    Log.e(TAGapp, "Json parsing error: " + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    Log.i(GET_PRODUCT_TAG, "Json parsing error: " + e.getMessage());
 
                 }
 
             } else {
-                Log.e(TAGapp, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                "Couldn't get json from server. Check LogCat for possible errors!",
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
+                Log.i(GET_PRODUCT_TAG, "Couldn't get json from server.");
             }
 
-            return null;
+            return result;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(getApplicationContext(),
-                            productName,
-                            Toast.LENGTH_LONG).show();
-                }
-            });
+        protected void onPreExecute() {
+            super.onPreExecute();
+
         }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            toReviewer(result);
+        }
+
     }
 }
 
--- app/src/main/java/com/biy/reviewvia/ReviewsActivity.java
+++ app/src/main/java/com/biy/reviewvia/ReviewsActivity.java
@@ -2,6 +2,7 @@ package com.biy.reviewvia;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -22,8 +24,8 @@ import java.util.ArrayList;
 
 public class ReviewsActivity extends AppCompatActivity {
 
+    private String mProductName;
     private TextView tvProductName;
-    private RecyclerView mRecyclerView;
     private ImageView mImageView;
     private RatingBar mRatingBar;
 
@@ -35,15 +37,17 @@ public class ReviewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
-        mRecyclerView = findViewById(R.id.recycler_view);
+        mProductName = getIntent().getStringExtra("product_name");
 
-        mRecyclerView.setHasFixedSize(true);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(layoutManager);
 
 
         mAdapter = new RecyclerViewAdapter(mList,this);
-        mRecyclerView.setAdapter(mAdapter);
+        recyclerView.setAdapter(mAdapter);
 
         tvProductName = findViewById(R.id.tv_produtname);
 
@@ -58,46 +62,67 @@ public class ReviewsActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                final StringBuilder builder = new StringBuilder();
+                String targetUrl = "https://www.productreview.com.au/search?q=" + Uri.encode(mProductName);
 
                 try{
-                    Document doc = Jsoup.connect("https://www.productreview.com.au/listings/bose-quietcomfort-35-qc35-ii").get();
-                    final Element titleElement = doc.selectFirst("#content > div:nth-child(4) > div.flex-column_36B.d-flex_b9D > div.flex-grow-1_3A8.relative_2e- > div > div.mb-3_2I3.card_134.card-full_3wf.card-full-md_2gh > div > div > div > div.align-items-stretch_26k.align-items-md-start_YBp.flex-column_36B.flex-md-row_2Sy.media_xHn > div.media-body_sNW > div.align-items-start_3CC.flex-column_36B.d-flex_b9D > h1 > span > span");
-                    final Elements reviewElements = doc.getElementsByAttributeValue("itemprop", "review");
-
-                    handleReviewItems(reviewElements);
-
-
-
-                    final Element imageElment = doc.selectFirst("#content > div:nth-child(4) > div.flex-column_36B.d-flex_b9D > div.flex-grow-1_3A8.relative_2e- > div > div.mb-3_2I3.card_134.card-full_3wf.card-full-md_2gh > div > div > div > div.align-items-stretch_26k.align-items-md-start_YBp.flex-column_36B.flex-md-row_2Sy.media_xHn > div.mr-0_1y6.mr-md-5_Y3v.mb-4_1be.mb-md-0_3-U.cursor--pointer_1MN.card_134.container_2PS > div > img");
-                    final Element ratingElement = doc.selectFirst("#content > div:nth-child(4) > div.flex-column_36B.d-flex_b9D > div.flex-grow-1_3A8.relative_2e- > div > div.mb-3_2I3.card_134.card-full_3wf.card-full-md_2gh > div > div > div > div.align-items-stretch_26k.align-items-md-start_YBp.flex-column_36B.flex-md-row_2Sy.media_xHn > div.media-body_sNW > div.mt-4_3rb.align-items-center_nNY.flex-wrap_1Wl.cursor--pointer_1MN.d-inline-flex_2a2 > div.flex-wrap_1Wl.d-flex_b9D > span > span.text-dark_3qN.font-size-lg_3Ta.font-weight-bold_xjV > span");
-
-                    String src = imageElment.attr("src");
-
-                    final URL url = new URL(src);
-                    final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-
-                    if (titleElement != null) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                tvProductName.setText(titleElement.text());
-                                mAdapter.notifyDataSetChanged();
-                                mImageView.setImageBitmap(bmp);
-                                mRatingBar.setRating(Float.parseFloat(ratingElement.text()));
-
-                            }
-                        });
-
-                    }
-                }catch (IOException e){
-
+                    Document doc = Jsoup.connect(targetUrl).get();
+                    handleResultList(doc);
+                } catch (IOException e){
+                    e.printStackTrace();
+                    toastError("Something went wrong");
                 }
             }
         }).start();
     }
 
-    void handleReviewItems(Elements reviewElements) {
+    private void handleResultList(Document searchResultDoc) {
+        final Element firstResult = searchResultDoc.selectFirst("a[href^=/listings/]");
+        if (firstResult == null) {
+            toastError("No matching data");
+        } else {
+            String firstItemUrl = firstResult.attr("href");
+            handleReviewDetail(firstItemUrl);
+        }
+    }
+
+    private void handleReviewDetail(String itemUrl) {
+        try {
+            String targetUrl = "https://www.productreview.com.au" + itemUrl;
+            Document doc = Jsoup.connect(targetUrl).get();
+            final Element titleElement = doc.selectFirst("meta[name=keywords]");
+
+            if (titleElement != null) {
+                final String productTitle = titleElement.attr("content");
+                final Elements reviewElements = doc.getElementsByAttributeValue("itemprop", "review");
+                handleReviewItems(reviewElements);
+                final String avatarUrl = doc.selectFirst("meta[itemprop=image]").attr("content");
+                final Element ratingElement = doc.selectFirst("div[itemprop=aggregateRating]").selectFirst("meta[itemprop=ratingValue]");
+
+
+                final URL url = new URL(avatarUrl);
+                final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvProductName.setText(productTitle);
+                        mAdapter.notifyDataSetChanged();
+                        mImageView.setImageBitmap(bmp);
+                        mRatingBar.setRating(Float.parseFloat(ratingElement.attr("content")));
+
+                    }
+                });
+            } else {
+                toastError("No matching data");
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            toastError("Something went wrong");
+        }
+
+    }
+
+    private void handleReviewItems(Elements reviewElements) {
         for(Element element : reviewElements) {
             final Element authorElement = element.selectFirst("div>div:first-child>div:first-child h4");
              String date = element.select("meta").attr("itemprop", "datePublished").get(1).attr("content");
@@ -111,6 +136,15 @@ public class ReviewsActivity extends AppCompatActivity {
             Log.d("review",reviewElement.text());*/
         }
 
+    }
+
+    private void toastError(final String content) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(ReviewsActivity.this, content, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
