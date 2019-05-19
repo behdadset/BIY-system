e542c30c6f19940bc1f59995df51b6c478d35b1f
nicolezhangyt <nicolezhangyt91@gmail.com>
2019-05-20 00:24:42 +1000

    1. fixed data extract; 2. add firebase functionality

---
+++ app/src/main/java/com/biy/reviewvia/HistoryAdapter.java
@@ -0,0 +1,10 @@
+package com.biy.reviewvia;
+
+/**
+ * @author Yating Zhang
+ * Email : nicolezhangyt91@gmail.com
+ * Date : 19/5/19
+ * Description :
+ */
+public class HistoryAdapter {
+}
---
+++ app/src/main/java/com/biy/reviewvia/Review.java
@@ -0,0 +1,10 @@
+package com.biy.reviewvia;
+
+/**
+ * @author Sean Zhu
+ * Email : seanzhuwx@gmail.com
+ * Date : 19/5/19
+ * Description :
+ */
+public class Review {
+}
