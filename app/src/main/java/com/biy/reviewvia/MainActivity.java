f8f44c4a9700ea9f238e5b038cd2f994fc7b74f2
behdadset <behdadsetoodegan@gmail.com>
2019-05-26 23:30:24 +1000

    UI and back button
    double tap on back button to exit and GUI changed.

--- app/src/main/java/com/biy/reviewvia/MainActivity.java
+++ app/src/main/java/com/biy/reviewvia/MainActivity.java
@@ -191,6 +191,18 @@ public class MainActivity extends AppCompatActivity {
             toReviewer(result);
         }
 
+
+
+    }
+
+    private static long back_pressed;
+
+    @Override
+    public void onBackPressed()
+    {
+        if (back_pressed + 2000 > System.currentTimeMillis()) super.onBackPressed();
+        else Toast.makeText(getBaseContext(), "Press once again to exit!", Toast.LENGTH_SHORT).show();
+        back_pressed = System.currentTimeMillis();
     }
 }
 
